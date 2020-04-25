9102bd637da6c55d468e84188e737ca195987f1a
Xell <menh.keo@gmail.com>
2020-04-25 14:50:43 +0700

    Initial Commit

---
+++ What is in my fridge ?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What is in my fridge ?
+//
+//  Created by Xell on 25/4/2563 BE.
+//  Copyright © 2563 Xell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ What is in my fridge ?Tests/What_is_in_my_fridge__Tests.swift
@@ -0,0 +1,34 @@
+//
+//  What_is_in_my_fridge__Tests.swift
+//  What is in my fridge ?Tests
+//
+//  Created by Xell on 25/4/2563 BE.
+//  Copyright © 2563 Xell. All rights reserved.
+//
+
+import XCTest
+@testable import What_is_in_my_fridge__
+
+class What_is_in_my_fridge__Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
